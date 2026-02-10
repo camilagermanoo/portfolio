db3e9c7af6dbfad358892aa6aa9566c4b98f769d
Camila Germano Rodrigues <camyrodrigues23@gmail.com>
2026-02-10 15:46:15 -0300

    adicionando background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackgound } from "../components/StarBackground"
 
 export const Home = () => {
     return( 
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             <ThemeToggle/>
+            <StarBackgound/>
 
 
         </div>
